0a04107828adcabbd4440a525259eb5757e73dbd
LucasLange <lucaslange@rede.ulbra.br>
2022-04-24 21:16:29 -0300

    Icons refator

--- lib/layout/app_bar_common_layout.dart
+++ lib/layout/app_bar_common_layout.dart
@@ -15,7 +15,7 @@ class AppBarCommonLayout extends AppBar {
           ),
           title: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+            children: <Widget>[
               Image.asset(
                 "assets/images/aula-logo.png",
                 fit: BoxFit.contain,
